d742fae7addf133fa990de966da554d8bbb838b8
Herson Oliveira <v-holiveira@hotels.com>
2019-09-06 16:47:59 +0200

    Add properties

--- week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -24,13 +24,41 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
-  property("min1") = forAll { a: A =>
+  property("min1") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
   }
 
-  property("min2") = forAll { (a: A), (b: A) =>
-
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    if (a > b) findMin(h) == b
+    else findMin(h) == a
   }
+
+  property("deleteMin") = forAll { a: Int =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("sorted") = forAll { h: H =>
+    def loop(heap: H, res: List[Int]): List[Int] = {
+      if (heap == empty) res
+      else loop(deleteMin(heap), findMin(heap) :: res)
+    }
+
+    val x = loop(h, Nil)
+    x == x.sorted
+  }
+
+  property("min of melding") = forAll { (a: H, b: H) =>
+    val min1 = findMin(a)
+    val min2 = findMin(b)
+    val h = meld(a, b)
+    val min = findMin(h)
+    min == min1 || min == min2
+//    val min = if (min1 > min2) min2 else min1
+//    findMin(h) == min
+  }
+
 
 }
